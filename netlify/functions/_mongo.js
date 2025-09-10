7c9a870710a6c5c5f1acce424c47ff2370e7a877
DaGzzz00 <ggwp30004@gmail.com>
2025-09-10 22:07:12 +0800

    first commit

---
+++ netlify/functions/_mongo.js
@@ -0,0 +1,31 @@
+// netlify/functions/_mongo.js
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.MONGODB_URI; // pulled from Netlify env variables
+if (!uri) {
+  throw new Error("MONGODB_URI environment variable not set");
+}
+
+let cachedClient = null;
+let cachedDb = null;
+
+async function connectToDatabase(dbName = "loanapp") {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  await client.connect();
+  const db = client.db(dbName);
+
+  cachedClient = client;
+  cachedDb = db;
+
+  return { client, db };
+}
+
+module.exports = { connectToDatabase };
